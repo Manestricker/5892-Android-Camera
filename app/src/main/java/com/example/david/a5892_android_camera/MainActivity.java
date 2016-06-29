9485303bf66779149f32a2e3bc60f27f8d357473
Manestricker <manestricker@gmail.com>
2016-06-29 12:21:32 -0500

    Revert "Revert "Revert "Added OpenCV libraries into Android Studio."""
    This reverts commit e14ecfc0b0ece590b64e971898f35123f84636eb.

--- app/src/main/java/com/example/david/a5892_android_camera/MainActivity.java
+++ app/src/main/java/com/example/david/a5892_android_camera/MainActivity.java
@@ -4,15 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //Loads opencv Libraries
-    static {
-        System.loadLibrary("opencv_java3");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
-        @Override
-        protected void onCreate (Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-        }
-
 }
-
