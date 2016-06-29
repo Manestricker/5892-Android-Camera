e5a21526828f532196d229053ef251d9f31f14a0
Manestricker <manestricker@gmail.com>
2016-06-29 12:03:27 -0500

    Added OpenCV libraries into Android Studio.

--- app/src/main/java/com/example/david/a5892_android_camera/MainActivity.java
+++ app/src/main/java/com/example/david/a5892_android_camera/MainActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    //Loads opencv Libraries
+    static {
+        System.loadLibrary("opencv_java3");
     }
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+
 }
+
